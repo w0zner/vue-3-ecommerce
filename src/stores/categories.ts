55bba1831f4509ade728fe584945393f5086b58b
cesar_ramirez <cesar_ramirez@hacienda.gov.py>
2024-06-07 11:49:04 -0400

    Se consume los datos del json

--- src/stores/categories.ts
+++ src/stores/categories.ts
@@ -1,13 +1,9 @@
 import type { Category } from "@/model/Type";
 import { defineStore } from "pinia";
-
+import data from '../../public/data/categories.json';
 
 export const useCategoriesStore = defineStore('categories', {
     state: () => ({
-        categories: [
-            {id: 1, name: "Oficina", description: "Productos para la oficina"},
-            {id: 2, name: "Tecnología", description: "Productos tecnológicos para el hogar"},
-            {id: 3, name: "Juguetes", description: "Juguetes para niños mayores a 3 años"}
-        ] as Category[]
+        categories: data as Category[]
     })
 })
--- src/stores/products.ts
+++ src/stores/products.ts
@@ -1,19 +1,12 @@
 import type { Product } from '@/model/Type'
 import { defineStore } from 'pinia'
+import data from '../../public/data/products.json';
 
 export const useProductsStore = defineStore('products', {
     state: () => ({
         order: '' as string,
         categoryId: null as number | null,
-        _products : [
-            {id:1, name: 'Silla', price: 26, image: '/products/silla.webp', categoryId: 1},
-            {id:2, name: 'Monitor', price: 450, image: '/products/monitor.jpg', categoryId: 2},
-            {id:3, name: 'Mouse', price: 10, image: '/products/mouse.jpg', categoryId: 2},
-            {id:4, name: 'Auricular', price: 30, image: '/products/headphones.jpg', categoryId: 2},
-            {id:5, name: 'Parlante', price: 40, image: '/products/parlantes.jpg', categoryId: 2},
-            {id:6, name: 'Miccrofóno', price: 15, image: '/products/microfono.jpg', categoryId: 2},
-            {id:7, name: 'Organizador', price: 8, image: '/products/organizador.webp', categoryId: 1}
-        ] as Product[]
+        _products : data as Product[]
     }),
     getters: {
         products(state) {
